406540477297f61409d89f494987399153593823
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-08-30 12:20:45 -0300

    Alteração config. swagger

--- src/main/java/br/com/codenation/log/config/SpringFoxConfig.java
+++ src/main/java/br/com/codenation/log/config/SpringFoxConfig.java
@@ -15,8 +15,8 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.com.codenation"))
+                .paths(PathSelectors.ant("/**"))
                 .build();
     }
 }
