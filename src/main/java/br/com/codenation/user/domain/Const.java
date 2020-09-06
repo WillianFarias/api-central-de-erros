9ba4f87f9d3fd70e36e6195de07137261d88f66b
WillianFarias <williansousafarias@gmail.com>
2020-09-06 10:26:21 -0300

    MyUserDetailsService, determina como vai funcionar a autenticacao

--- src/main/java/br/com/codenation/user/domain/Const.java
+++ src/main/java/br/com/codenation/user/domain/Const.java
@@ -1,6 +1,8 @@
 package br.com.codenation.user.domain;
 
 public class Const {
+
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String ROLE_CLIENT = "ROLE_CLIENT";
+
 }
