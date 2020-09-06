7ec75bd63ce7ccb63bf24000961f9ad39a3dbb23
WillianFarias <williansousafarias@gmail.com>
2020-09-05 22:57:45 -0300

    DataInitializr, responsavel por criar dados iniciais

--- src/main/java/br/com/codenation/user/config/DataInitializr.java
+++ src/main/java/br/com/codenation/user/config/DataInitializr.java
@@ -1,4 +1,46 @@
 package br.com.codenation.user.config;
 
-public class DataInitializr {
+import br.com.codenation.user.model.Role;
+import br.com.codenation.user.model.User;
+import br.com.codenation.user.repository.RoleRepository;
+import br.com.codenation.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataInitializr implements ApplicationListener<ContextRefreshedEvent> {
+
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_CLIENT = "ROLE_CLIENT";
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent arg0) {
+        List<User> users = userRepository.findAll();
+
+        if (users.isEmpty()){
+            createUser("Admin", "admin", passwordEncoder.encode("123456"), ROLE_ADMIN);
+            createUser("Cliente", "cliente", passwordEncoder.encode("123456"), ROLE_CLIENT);
+        }
+    }
+
+    private void createUser(String name, String email, String password, String roleName) {
+        Role role = new Role(roleName);
+
+        this.roleRepository.save(role);
+        User user = new User(name, email, password, Arrays.asList(role));
+        userRepository.save(user);
+    }
 }
