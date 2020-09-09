4132ff6e0145bd9cf611acb9948529ee1bec9dee
WillianFarias <williansousafarias@gmail.com>
2020-09-09 09:21:44 -0300

    UserController

---
+++ src/main/java/br/com/codenation/user/controller/UserController.java
@@ -0,0 +1,50 @@
+package br.com.codenation.user.controller;
+
+import br.com.codenation.user.domain.Const;
+import br.com.codenation.user.model.User;
+import br.com.codenation.user.repository.RoleRepository;
+import br.com.codenation.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Secured({Const.ROLE_ADMIN})
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<User> save(@RequestBody User user) {
+        user = this.userRepository.save(user);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @Secured({Const.ROLE_ADMIN})
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public ResponseEntity<User> edit(@RequestBody User user){
+        user = this.userRepository.save(user);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Page<User>> list(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
+    ){
+        Pageable pageable = PageRequest.of(page, size, Sort.by("name"));
+        return new ResponseEntity<User>(userRepository.findAll(pageable), HttpStatus.OK);
+    }
+}
